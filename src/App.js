b1edc106e67ecbbb6d3d74faab5a4a65c3ef7ad1
NataliaSacias <nsacias@gmail.com>
2022-01-18 21:46:34 -0300

    created the button to add the list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import createList from './components/Create_List';
 
 import './App.css';
 
 function App() {
   return (
     <main>
+      <createList />
       <h1>Shopping List!</h1>
     </main>
   );
---
+++ src/components/Create_List.js
@@ -0,0 +1,11 @@
+import {
+  getToken,
+  words,
+  calculateEstimate,
+} from '@the-collab-lab/shopping-list-utils';
+
+const createList = () => {
+  return <button>Create new list</button>;
+};
+
+export default createList;
