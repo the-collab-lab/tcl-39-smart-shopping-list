d9fded01da0491473942dae7e100eca0cb578911
Dara97 <dara.inampuez@gmail.com>
2022-01-19 17:53:27 -0500

    token added to firebase

--- src/components/NewToken.js
+++ src/components/NewToken.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { getToken, words } from '@the-collab-lab/shopping-list-utils';
 import { useNavigate } from 'react-router-dom';
+import { collection, addDoc } from 'firebase/firestore';
+import { db } from '../lib/firebase';
+import { doc, setDoc } from 'firebase/firestore';
 
 const NewToken = () => {
   let navigate = useNavigate();
-  function token() {
+
+  function token(products) {
+    const getTokenStorage = getToken(words);
+    localStorage.setItem('token', JSON.stringify(getTokenStorage));
+
+    setDoc(doc(db, 'Tokens', getTokenStorage), {});
+
     navigate('/list');
-    localStorage.setItem('token', JSON.stringify(getToken(words)));
-    const getStorage = JSON.parse(localStorage.getItem('token'));
   }
+
   return (
     <>
       <button onClick={token}>Create a new list</button>
