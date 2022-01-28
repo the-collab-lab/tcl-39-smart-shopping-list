cb6f56c23b6b8bf6813c596dc5dc5b6ebb8a50ff
Elizabeth <elizabethriver@gmail.com>
2022-01-28 12:54:26 -0500

    update code functions to 1 line

--- src/components/modal/ModalFunctions.js
+++ src/components/modal/ModalFunctions.js
@@ -3,12 +3,8 @@ import { useState } from 'react';
 //set state class to modal 'Successfully Product Added Msg'
 export const useModalFunctions = () => {
   const [modalClass, setmodalClass] = useState(false);
-  const showModal = () => {
-    setmodalClass(true);
-  };
-  const hideModal = () => {
-    setmodalClass(false);
-  };
+  const showModal = () => setmodalClass(true);
+  const hideModal = () => setmodalClass(false);
   return {
     modalClass,
     showModal,
