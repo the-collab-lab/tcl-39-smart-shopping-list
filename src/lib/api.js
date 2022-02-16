bed26d64b90a8306733c7e054a7b0d2eedbf3d26
jpeg25 <jairo25509@hotmail.com>
2022-02-16 08:44:09 -0500

    sort items in groups alphabetically.

--- src/lib/api.js
+++ src/lib/api.js
@@ -38,8 +38,22 @@ export const getItemsFromList = async (token) => {
 
   if (itemsFromList) {
     itemsFromList.sort((a, b) => {
+      let fa = a.name.toLowerCase(),
+        fb = b.name.toLowerCase();
+
+      if (fa < fb) {
+        return -1;
+      }
+      if (fa > fb) {
+        return 1;
+      }
+      return 0;
+    });
+
+    itemsFromList.sort((a, b) => {
       return a.howSoon - b.howSoon;
     });
+
     console.log(itemsFromList);
     return itemsFromList;
   }
