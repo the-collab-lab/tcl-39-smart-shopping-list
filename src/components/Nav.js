955d19f7ea53a341e8169ab9068099a20c78e2af
Elizabeth <elizabethriver@gmail.com>
2022-02-24 22:47:44 -0500

    install material ui and top nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,31 +1,37 @@
-import React from 'react';
+import * as React from 'react';
 import { NavLink } from 'react-router-dom';
 import './Nav.css';
 
 export const Nav = () => {
+
   return (
     <nav>
       <NavLink
         className="nav-link"
         style={({ isActive }) => ({
-          fontWeight: isActive ? 600 : 400,
-          color: isActive ? '#000' : '#AAA7AA',
-          background: isActive ? '#B39CD0' : '#FBEAFF',
+          color: isActive ? '#6691ED' : '#DEDEDE',
         })}
         to="/list"
       >
-        List
+        <span class="material-icons md-36">view_list</span>
       </NavLink>
       <NavLink
-        className={'nav-link'}
+        className="nav-link"
         style={({ isActive }) => ({
-          fontWeight: isActive ? 600 : 400,
-          color: isActive ? '#000' : '#AAA7AA',
-          background: isActive ? '#B39CD0' : '#FBEAFF',
+          color: isActive ? '#6691ED' : '#DEDEDE',
         })}
         to="/add-items"
       >
-        Add Item
+        <span class="material-icons md-36">add_circle</span>
+      </NavLink>
+      <NavLink
+        className="nav-link"
+        style={({ isActive }) => ({
+          color: isActive ? '#6691ED' : '#DEDEDE',
+        })}
+        to="/"
+      >
+        <span class="material-icons md-36">exit_to_app</span>
       </NavLink>
     </nav>
   );
