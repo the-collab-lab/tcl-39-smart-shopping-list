93297f9ed04c465956bf587debe3cc3e771ec379
Elizabeth <elizabethriver@gmail.com>
2022-02-25 19:09:31 -0500

    clean loading component

--- src/components/loading/loading.js
+++ src/components/loading/loading.js
@@ -1,17 +1,12 @@
-import { css } from '@emotion/react';
 import PuffLoader from 'react-spinners/PuffLoader';
 import "./loading.css"
 
-// Can be a string as well. Need to ensure each key-value pair ends with ;
 const color = '#7A7A7A'
-const override = css`
-  // top: 50%;
-`;
 
 const Loading = () => {
   return (
     <div className='container-loading'>
-      <PuffLoader size={160} color={color} css={override} />
+      <PuffLoader size={160} color={color} />
       <p className='loading_text'>Loading</p>
     </div>
   );
