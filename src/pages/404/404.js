65fa46d90cc6b4948d266a617adacfafb3f56ed0
Elizabeth <elizabethriver@gmail.com>
2022-02-27 14:38:28 -0500

    nav

--- src/pages/404/404.js
+++ src/pages/404/404.js
@@ -1,19 +1,22 @@
 import { useNavigate } from 'react-router-dom';
+import "./404.css"
 
 export const NotFound404 = () => {
   const navigate = useNavigate();
   return (
-    <>
-      <h1>404 Not Found Route</h1>
-      <h3>Looks like you're lost!!! 😱</h3>
-      <h4>Fear no more, we can help you 🏡</h4>
+    <main>
+      <h1 className='h1-container'>Oops!</h1>
+      <h2>404 Not Found Route</h2>
+      <p>Looks like you're lost!!! 😱</p>
+      <p>Fear no more, we can help you 🏡</p>
       <button
+        className='button-container'
         onClick={() => {
           navigate('/');
         }}
       >
         Click to go home
       </button>
-    </>
+    </main>
   );
 };
