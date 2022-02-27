a26ed5cae5bb8cdf1d2deb4b13feee4560272047
Elizabeth <elizabethriver@gmail.com>
2022-02-27 16:38:29 -0500

    sytling 404page

--- src/pages/404/404.js
+++ src/pages/404/404.js
@@ -4,11 +4,11 @@ import "./404.css"
 export const NotFound404 = () => {
   const navigate = useNavigate();
   return (
-    <main>
+    <main className='main-404-container'>
       <h1 className='h1-container'>Oops!</h1>
-      <h2>404 Not Found Route</h2>
-      <p>Looks like you're lost!!! 😱</p>
-      <p>Fear no more, we can help you 🏡</p>
+      <h2 className='h2-container'>404 Not Found Route</h2>
+      <p className='p-container'>Looks like you're lost!!! 😱</p>
+      <p className='p-container'>Fear no more, we can help you 🏡</p>
       <button
         className='button-container'
         onClick={() => {
